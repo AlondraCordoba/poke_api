547e57e8ee632fc8ff311932026cce095facbf8b
AlondraCordoba <alondracordoba0720@gmail.com>
2022-01-04 13:32:26 -0600

    .

--- test/poke_api/api_client_test.rb
+++ test/poke_api/api_client_test.rb
@@ -23,41 +23,21 @@ module PokeApi
 
     def test_client_must_get_pokemons
       VCR.use_cassette('pokemons') do
-        # assert_instance_of Hash, client.get('pokemon')
         assert_instance_of Array, client.get('pokemon')[:results]
       end
     end
 
-    # def test_client_must_get_pokemon
-    #   VCR.use_cassette('pokemon') do
-    #     assert_instance_of Hash, client.get('pokemon/1')
-    #   end
-    # end
-
     def test_client_must_get_types
       VCR.use_cassette('types') do
-        # assert_instance_of Hash, client.get('type')
         assert_instance_of Array, client.get('type')[:results]
       end
     end
 
-    # def test_client_must_get_type
-    #   VCR.use_cassette('type') do
-    #     assert_instance_of Hash, client.get('type/1')
-    #   end
-    # end
-
     def test_client_must_get_abilities
       VCR.use_cassette('abilities') do
-        # assert_instance_of Hash, client.get('ability')
         assert_instance_of Array, client.get('ability')[:results]
       end
     end
 
-    # def test_client_must_get_ability
-    #   VCR.use_cassette('ability') do
-    #     assert_instance_of Hash, client.get('ability/1')
-    #   end
-    # end
   end
 end
